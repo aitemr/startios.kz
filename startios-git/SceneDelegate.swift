47676fb59ea174d04a187b8494940607faeab796
islam.t <islam.t@snoonu.com>
2023-02-07 20:34:02 +0600

    удалил лишний код

--- startios-git/SceneDelegate.swift
+++ startios-git/SceneDelegate.swift
@@ -8,9 +8,7 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
@@ -43,7 +41,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
 
